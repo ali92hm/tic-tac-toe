24f7d4a511368eace2b49a3089e1fee39353da9f
ali92hm <ali92hm@gmail.com>
2017-10-30 12:00:33 -0700

    added mongoose connection options

--- config/default.js
+++ config/default.js
@@ -6,6 +6,17 @@ module.exports = {
   env: env,
   isProduction: env === 'production',
   port: process.env.PORT || 3000,
+  db: {
+    mongo: {
+      uri: process.env.MONGODB_URI || 'mongodb://db/default',
+      options: {
+        useMongoClient: true,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 500,
+        poolSize: 5
+      }
+    }
+  },
   gameCommands: {
     displayBoard: 'display',
     challenge: 'challenge',
