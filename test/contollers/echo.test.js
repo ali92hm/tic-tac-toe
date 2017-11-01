fb520dbe7dd4b493ffeed690601b284ae93cff72
ali92hm <ali92hm@gmail.com>
2017-10-31 18:57:34 -0700

    updated tests

--- test/contollers/echo.test.js
+++ test/contollers/echo.test.js
@@ -1,7 +1,42 @@
 const echoController = require('../../app/controllers/echo')
 
-describe('testing functions in echo controller', () => {
-  test('Vienna <3 sausage', () => {
+describe('requestValidatorIndex(method, query, body)', () => {
+  var method
+  var queryString
+  var body
+  beforeEach(() => {
+    method = 'POST'
+    queryString = '?name=you&&this=that'
+    body = {
+      a: 'somevalue',
+      b: 2,
+      c: true
+    }
+  })
+
+  test('should undefined for complete params', () => {
+    expect(echoController.requestValidatorIndex(method, queryString, body))
+      .toBeUndefined()
+  })
+
+  test('should undefined for missing optional param queryString', () => {
+    expect(echoController.requestValidatorIndex(method, undefined, body))
+      .toBeUndefined()
+  })
+
+  test('should return error for missing method', () => {
+    expect(echoController.requestValidatorIndex(undefined, queryString, body))
+      .toEqual(['No "method" was passed.'])
+  })
+
+  test('should return error for wrong method', () => {
+    expect(echoController.requestValidatorIndex('SOMEMETHOD', queryString, body))
+      .toEqual(['"SOMEMETHOD" is not an allowed method.'])
+  })
+})
+
+describe('echo(method, query, body)', () => {
+  test('it should return everything that is passed to it', () => {
     let method = 'somemethod'
     let queryString = '?name=you&&this=that'
     let body = {
