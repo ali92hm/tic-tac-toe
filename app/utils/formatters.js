117eee2cf3037d8f78af9b2594dddc7a52397cc8
ali92hm <ali92hm@gmail.com>
2017-11-01 22:42:27 -0700

    added formatters

---
+++ app/utils/formatters.js
@@ -0,0 +1,45 @@
+const slackResponseFormatter = (text, attachments, error, join = '\n') => {
+  if (Array.isArray(text)) {
+    text = text.join(join)
+  }
+
+  let response = {
+    text: text,
+    attachments: attachments,
+    response_type: 'in_channel'
+  }
+
+  if (error) {
+    delete response.response_type
+  }
+
+  return response
+}
+
+const boardAsciiFormatter = (board) => {
+  for (let i = 0; i < board.length; i++) {
+    board[i] = board[i] || (i + 1)
+  }
+  let [ v1, v2, v3, v4, v5, v6, v7, v8, v9 ] = board
+
+  let template = `
+  +---+---+---+
+  | ${v1} | ${v2} | ${v3} |
+  |---+---+---|
+  | ${v4} | ${v5} | ${v6} |
+  |---+---+---|
+  | ${v7} | ${v8} | ${v9} |
+  +---+---+---+`
+
+  return template
+}
+
+const slackErrorFormatter = (messages, join = '\n') => {
+  return slackResponseFormatter(messages, null, true, join)
+}
+
+module.exports = {
+  slackResponseFormatter,
+  boardAsciiFormatter,
+  slackErrorFormatter
+}
