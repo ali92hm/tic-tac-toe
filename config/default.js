eefccfa83e4643151637a3aad24e9532368c8da8
ali92hm <ali92hm@gmail.com>
2017-11-01 22:41:24 -0700

    added game and messaging constants

--- config/default.js
+++ config/default.js
@@ -17,11 +17,28 @@ module.exports = {
       }
     }
   },
-  gameCommands: {
-    default: 'DEFAULT',
-    displayBoard: 'DISPLAY',
-    challenge: 'CHALLENGE',
-    place: 'PLACE',
-    help: 'HELP'
+  game: {
+    xSymbole: 'X',
+    oSymbole: 'O',
+    drawSymbole: 'Draw'
+  },
+  slack: {
+    commands: {
+      default: 'DEFAULT',
+      displayBoard: 'DISPLAY',
+      challenge: 'CHALLENGE',
+      place: 'PLACE',
+      help: 'HELP'
+    },
+    messages: {
+      noGameInProgress: ['Oops no game in progress!!', 'You can start one by saying `/ttt challenge @name`'],
+      gameInProgress: ['There is a game in progress for this channel :cry:', 'Please try another channel'],
+      noOpponent: ['No opponent specified.', 'Please use `/ttt challenge @username`'],
+      challengeSelf: ['You cant challenge yourself.', 'Please pick someone else as opponent'],
+      helpMessage: [ 'Hello :wave: Welcome to Tic-Tac-Toe Slack game.',
+        'Type `/ttt challenge @name` to start a game.',
+        'Type `/ttt display` to display the ongoing game.',
+        'Type `/ttt place 1-9` to place a move.']
+    }
   }
 }
