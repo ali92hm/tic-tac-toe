b30118dcc8b6817225c959ab14c7f36de7df6d37
ali92hm <ali92hm@gmail.com>
2017-11-05 08:57:51 -0800

    added more parsers

--- app/utils/parsers.js
+++ app/utils/parsers.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const Errors = require('./errors')
 const isUserRegx = /<@(U|W).+>/i
 
 const parseCommand = (text, defaultCommand = '') => {
@@ -31,9 +32,28 @@ const parseCommand = (text, defaultCommand = '') => {
 }
 
 const parseSlackUserId = (token) => {
-  if (isUserRegx.test(token)) {
-    return normalizeString(_.first(token.match(/[a-z0-9]+/i)))
+  if (!isUserRegx.test(token)) {
+    throw new Errors.SlackNotUserIdError(`"${token}" is not a slack userId`)
   }
+
+  return normalizeString(_.first(token.match(/[a-z0-9]+/i)))
+}
+
+const getOnlyItem = (array) => {
+  if (array.length !== 1) {
+    throw new Errors.ArgumentError('More than one argument was passed')
+  }
+
+  return _.first(array)
+}
+
+const parseWholeNumber = (string) => {
+  // TODO: Turn this into regex
+  if (string.indexOf('.') !== -1) {
+    throw new Errors.NotIntegerError(`"${string}" is not a whole number`)
+  }
+
+  return parseInt(string)
 }
 
 const normalizeString = (string) => {
@@ -47,5 +67,7 @@ const normalizeString = (string) => {
 module.exports = {
   parseSlackUserId,
   normalizeString,
-  parseCommand
+  getOnlyItem,
+  parseCommand,
+  parseWholeNumber
 }
