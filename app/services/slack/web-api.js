c234fd050310268f0354b1390f7bf7019952005d
ali92hm <ali92hm@gmail.com>
2017-11-05 15:44:01 -0800

    added docs + custom error

--- app/services/slack/web-api.js
+++ app/services/slack/web-api.js
@@ -1,6 +1,14 @@
 const config = require('config')
 const requests = require('../../utils/requests')
+const Errors = require('../../utils/errors')
 
+/*
+* Get user info from slack api
+* @async
+* @param {string} teamId - normalized slack teamId
+* @param {string} channelId - normalized slack channelId
+* @returns {Promise<Object>} userInfo - retrieved user info from slack
+*/
 const getUserInfo = async (teamId, userId) => {
   let response = await slackRequestManaget(teamId, 'users.info', {user: userId})
 
@@ -18,6 +26,14 @@ const getUserInfo = async (teamId, userId) => {
   }
 }
 
+/*
+* Wrapper for making request to slack API
+* @async
+* @param {string} teamId - normalized slack teamId
+* @param {string} urlExtension - the method extension url
+* @param {Object} fields - web request fields
+* @returns {Promise<Object>} result - result of the web request
+*/
 const slackRequestManaget = async (teamId, urlExtension, fields) => {
   let url = config.get('slack.baseApiUrl') + '/' + urlExtension
 
@@ -26,7 +42,7 @@ const slackRequestManaget = async (teamId, urlExtension, fields) => {
 
   let result = await requests.requestManager('POST', url, fields, token)
   if (!result.ok) {
-    throw new Error(result.error)
+    throw new Errors.SlackAPIError(result.error)
   }
 
   return result
