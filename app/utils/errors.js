a734f2a798a3fd61cb80d29061f41309a7c6b109
ali92hm <ali92hm@gmail.com>
2017-11-05 09:13:50 -0800

    added more error and sorted file

--- app/utils/errors.js
+++ app/utils/errors.js
@@ -1,50 +1,29 @@
+/******************
+* Argument Errors *
+*******************/
 
+class ArgumentError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class NotIntegerError extends ArgumentError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+/**************
+* Game Errors *
+***************/
 class GameError extends Error {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-class SamePlayersError extends GameError {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-class NoPlayerError extends GameError {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-class WrongPlayerError extends GameError {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-class NotTurnError extends GameError {
-  constructor (message) {
-    super(message)
-    this.name = this.constructor.name
-
-    Error.captureStackTrace(this, this.constructor)
-  }
-}
-
-class InvalidMove extends GameError {
   constructor (message) {
     super(message)
     this.name = this.constructor.name
@@ -62,12 +41,102 @@ class CellTakenError extends GameError {
   }
 }
 
+class InvalidMoveError extends GameError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class NoPlayerError extends GameError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class NotTurnError extends GameError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class SamePlayersError extends GameError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class WrongPlayerError extends GameError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+/***************
+* Slack Errors *
+****************/
+
+class SlackError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class SlacGameInProgress extends SlackError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class SlackNotUserIdError extends SlackError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+class SlackUknownCommandError extends SlackError {
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
 module.exports = {
+  ArgumentError,
+  CellTakenError,
   GameError,
-  WrongPlayerError,
-  SamePlayersError,
-  NotTurnError,
-  InvalidMove,
+  InvalidMoveError,
   NoPlayerError,
-  CellTakenError
+  NotIntegerError,
+  NotTurnError,
+  SamePlayersError,
+  SlacGameInProgress,
+  SlackNotUserIdError,
+  SlackUknownCommandError,
+  WrongPlayerError
 }
