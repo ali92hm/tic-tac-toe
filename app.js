30812dec1d50f57b34fae3cd5471a4249da765a7
ali92hm <ali92hm@gmail.com>
2017-11-05 11:18:23 -0800

    comments + start express after db connection is success

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const config = require('config')
 
 const app = express()
 
+// Configure winston
 require('./config/winston')(app, config)
-require('./config/mongo')(app, config)
+// Configure mongo
+let connection = require('./config/mongo')(app, config)
+// Configure
 require('./config/express')(app, config)
 
-app.listen(config.get('port'), () => {
-  winston.info('Express server listening on port ' + config.get('port'))
+// Start accepting http calls after mongoose is connected
+connection.on('connected', () => {
+  app.listen(config.get('port'), () => {
+    winston.info('Express server listening on port:', config.get('port'))
+  })
 })
 
 exports = module.exports = app
